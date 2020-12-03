ec2faafa29ab026e1b991caabe3a65c5848f3498
Ali Shah <s.ashah@outlook.com>
2020-12-03 01:38:01 -0500

    Finished results route and added some error trapping

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import requests
 
 from pprint import PrettyPrinter
-from datetime import datetime, timedelta
+from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
 from geopy.geocoders import Nominatim
 from flask import Flask, render_template, request, send_file
@@ -36,48 +36,51 @@ def home():
     }
     return render_template('home.html', **context)
 
-def get_letter_for_units(units):
+def get_units_for_temp(units):
     """Returns a shorthand letter for the given units."""
     return 'F' if units == 'imperial' else 'C' if units == 'metric' else 'K'
+
+def get_units_for_wind(units):
+    """Returns appropriate units for wind speed."""
+    return 'm/s' if units == 'metric' or units == 'kelvin' else 'mph'
 
 @app.route('/results')
 def results():
     """Displays results for current weather conditions."""
-    # TODO: Use 'request.args' to retrieve the city & units from the query
-    # parameters.
-    city = ''
-    units = ''
 
-    url = 'http://api.openweathermap.org/data/2.5/weather'
+    city = request.args.get('city')
+    units = request.args.get('units')
+    URL = 'http://api.openweathermap.org/data/2.5/weather'
     params = {
-        # TODO: Enter query parameters here for the 'appid' (your api key),
-        # the city, and the units (metric or imperial).
-        # See the documentation here: https://openweathermap.org/current
-
+        'appid': API_KEY,
+        'q': city,
+        'units': units
     }
 
-    result_json = requests.get(url, params=params).json()
+    result_json = requests.get(URL, params=params).json()
 
     # Uncomment the line below to see the results of the API call!
     # pp.pprint(result_json)
 
-    # TODO: Replace the empty variables below with their appropriate values.
-    # You'll need to retrieve these from the result_json object above.
-
     # For the sunrise & sunset variables, I would recommend to turn them into
     # datetime objects. You can do so using the `datetime.fromtimestamp()`
     # function.
-    context = {
-        'date': datetime.now(),
-        'city': '',
-        'description': '',
-        'temp': '',
-        'humidity': '',
-        'wind_speed': '',
-        'sunrise': '',
-        'sunset': '',
-        'units_letter': get_letter_for_units(units)
-    }
+    try:
+        t_zone = timezone(timedelta(hours=result_json['timezone']/3600))
+        context = {
+            'date': datetime.now(tz=t_zone),
+            'city': f"{result_json['name']}, {result_json['sys']['country']}",
+            'description': result_json['weather'][0]['description'],
+            'temp': result_json['main']['temp'],
+            'humidity': result_json['main']['humidity'],
+            'wind_speed': result_json['wind']['speed'],
+            'sunrise': datetime.fromtimestamp(result_json['sys']['sunrise'], tz=t_zone),
+            'sunset': datetime.fromtimestamp(result_json['sys']['sunset'], tz=t_zone),
+            'temp_units': get_units_for_temp(units),
+            'wind_units': get_units_for_wind(units)
+        }
+    except:
+        return render_template('error.html', city=city)
 
     return render_template('results.html', **context)
 
